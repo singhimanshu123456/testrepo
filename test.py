00c52e39dcab170cad9e53f6a071bbe3cc156ec3
hilnu <t-hilnu@expediagroup.com>
2019-06-26 15:56:52 +0530

    june

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import os
 
 os.system('git init')
 os.system('git remote set-url origin https://github.com/AnshDivya/himRepo.git')
-os.system('git config --global user.name "{}"'.format("hilnu"))
-os.system('git config --global user.email "{}"'.format("t-hilnu@expediagroup.com"))
+#os.system('git config --global user.name "{}"'.format("hilnu"))
+#os.system('git config --global user.email "{}"'.format("t-hilnu@expediagroup.com"))
 
 os.system("git add .")
 os.system('git commit -m "{}"'.format("june"))
