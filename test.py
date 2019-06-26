78e5c57b4151dde0ac29a5f58ebbad355bd870b4
hilnu <singhimanshu123456@gmail.com>
2019-06-26 15:37:24 +0530

    june

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
 os.system('git init')
-os.system('git remote add origin https://github.expedia.biz/divthakur/testRepo.git')
+os.system('git remote add origin git@github.expedia.biz:divthakur/testRepo.git')
 os.system("git add .")
 os.system('git commit -m "{}"'.format("june"))
 os.system("git push origin master")
