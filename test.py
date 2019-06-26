e0a5be1763e942c7d838cf8aca238923ca5d2738
hilnu <t-hilnu@expediagroup.com>
2019-06-26 15:49:52 +0530

    june

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import os
 
 os.system('git init')
 os.system('git remote add origin git@github.expedia.biz:divthakur/testRepo.git')
+os.system('git config --global user.name "{}"'.format("hilnu"))
+os.system('git config --global user.email "{}"'.format("t-hilnu@expediagroup.com"))
+os.system('git commit -m "{}"'.format("june"))
 os.system("git add .")
 os.system('git commit -m "{}"'.format("june"))
 os.system("git push origin master")
